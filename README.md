b1a1e82e86e9431c85dac3d1f10942701796db9d
Joshua Selbo <joshuaselbo@gmail.com>
2014-10-27 17:02:57 -0400

    Fixed up build scripts. Also fixed critical bug.

--- README.md
+++ README.md
@@ -3,5 +3,5 @@ MegaMan2
 
 Faithful recreation of MegaMan 2 in Java (always a work in progress)
 
-Thanks to Martin Hunter for certain stuff!
+Thanks to Hartin Munter for certain stuff!
 
---
+++ compile.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "Cleaning bin/ directory..."
+# Create and clean bin/
+mkdir -p bin
+rm -rf bin/*
+
+echo "Copying resources..."
+# Copy resources to bin/
+cp -r res bin
+
+echo "Compiling..."
+# Compile all Java files in src/
+find src -name "*.java" | xargs javac -d bin
+
